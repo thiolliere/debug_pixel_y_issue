98353b57b75b3ff96ce747f2ea47ab4b645693ef
thiolliere <gui.thiolliere@gmail.com>
2022-05-14 20:00:10 +0900

    fmt

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,39 @@
 use bevy::prelude::*;
 
 fn main() {
-	App::new()
-		.add_plugins(DefaultPlugins)
-		.add_startup_system(setup)
-		.run();
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .insert_resource(Msaa { samples: 1 })
+        .add_startup_system(setup)
+        .run();
 }
 fn setup(
-	mut commands: Commands,
-	asset_server: Res<AssetServer>,
-	mut texture_atlases: ResMut<Assets<TextureAtlas>>,
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut texture_atlases: ResMut<Assets<TextureAtlas>>,
 ) {
-	let texture_handle = asset_server.load("Minifantasy_TownsAllTiles.png");
-	let texture_atlas = TextureAtlas::from_grid(texture_handle, Vec2::new(8.0, 8.0), 64, 36);
-	let texture_atlas_handle = texture_atlases.add(texture_atlas);
+    let texture_handle = asset_server.load("Minifantasy_TownsAllTiles.png");
+    let texture_atlas = TextureAtlas::from_grid(texture_handle, Vec2::new(8.0, 8.0), 64, 36);
+    let texture_atlas_handle = texture_atlases.add(texture_atlas);
 
-	let s = 0.9; // when s is less than 1.0 it fails
-	commands.spawn_bundle(OrthographicCameraBundle::new_2d())
-		.insert(
-			Transform {
-				translation: Vec3::new(
-								 0.0,
-								 230.12491,
-								 0.0,
-							 ),
-							 scale: Vec3::new(
-								 1.0,
-								 s,
-								 1.0,
-							 ),
-							 ..Default::default()
-			});
+    commands
+        .spawn_bundle(OrthographicCameraBundle::new_2d())
+        .insert(Transform {
+            translation: Vec3::new(0.0, 230.12491, 0.0),
+            scale: Vec3::new(1.0, 0.25, 1.0),
+            ..Default::default()
+        });
 
-	for y in 0..100 {
-		commands
-			.spawn_bundle(SpriteSheetBundle {
-				sprite: TextureAtlasSprite::new(66),
-				texture_atlas: texture_atlas_handle.clone(),
-				transform: Transform {
-					translation: Vec3::new(0.0, y as f32 * 8.0 * s * 20.0, 0.0),
-					scale: Vec3::new(100.0, s * 20.0, 1.0),
-					..Default::default()
-				},
-					..Default::default()
-			});
-	}
+    for y in 0..100 {
+        commands.spawn_bundle(SpriteSheetBundle {
+            sprite: TextureAtlasSprite::new(66),
+            texture_atlas: texture_atlas_handle.clone(),
+            transform: Transform {
+                translation: Vec3::new(0.0, y as f32 * 8.0, 0.0),
+                scale: Vec3::new(100.0, 1.0, 1.0),
+                ..Default::default()
+            },
+            ..Default::default()
+        });
+    }
 }
-
-
