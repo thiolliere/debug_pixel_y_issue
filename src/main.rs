cd81b5a463b80639198e36ce46bd1ef0d75cb025
thiolliere <gui.thiolliere@gmail.com>
2022-05-15 12:42:27 +0900

    narrowing the issue

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,14 @@ fn main() {
         .add_startup_system(setup)
         .run();
 }
+
 fn setup(
     mut commands: Commands,
     asset_server: Res<AssetServer>,
     mut texture_atlases: ResMut<Assets<TextureAtlas>>,
 ) {
-    let texture_handle = asset_server.load("Minifantasy_TownsAllTiles.png");
-    let texture_atlas = TextureAtlas::from_grid(texture_handle, Vec2::new(8.0, 8.0), 64, 36);
+    let texture_handle = asset_server.load("Minifantasy_TownsAllTiles2.png");
+    let texture_atlas = TextureAtlas::from_grid(texture_handle, Vec2::new(1.0, 1.0), 3, 3);
     let texture_atlas_handle = texture_atlases.add(texture_atlas);
 
     commands
@@ -24,16 +25,14 @@ fn setup(
             ..Default::default()
         });
 
-    for y in 0..100 {
-        commands.spawn_bundle(SpriteSheetBundle {
-            sprite: TextureAtlasSprite::new(66),
-            texture_atlas: texture_atlas_handle.clone(),
-            transform: Transform {
-                translation: Vec3::new(0.0, y as f32 * 8.0, 0.0),
-                scale: Vec3::new(100.0, 1.0, 1.0),
-                ..Default::default()
-            },
+    commands.spawn_bundle(SpriteSheetBundle {
+        sprite: TextureAtlasSprite::new(4),
+        texture_atlas: texture_atlas_handle.clone(),
+        transform: Transform {
+            translation: Vec3::new(0.0, 240.0, 0.0),
+            scale: Vec3::new(1000.0, 100.0, 1.0),
             ..Default::default()
-        });
-    }
+        },
+        ..Default::default()
+    });
 }
